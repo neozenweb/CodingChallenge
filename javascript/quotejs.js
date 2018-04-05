13701dcf445c62ea7615d787a5f91f461c089dad
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-04-05 15:44:04 -0400

    Update quotejs.js

--- javascript/quotejs.js
+++ javascript/quotejs.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 								var ranqt = result.filter(function(){
 									var randnum = Math.floor(Math.random() * 10) +1;
 									alert("Random number is " + randnum);
-									var randomquote = JSON.stringify(ranqt);
+									var randomquote = JSON.stringify(result);
 									 alert("Random quote is  " +randomquote[randnum]);
 									return randomquote[randnum];
 								});
