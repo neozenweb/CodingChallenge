7bf81cfa8b93f43b77a082a7bd1335f97d2aecef
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-04-05 09:49:21 -0400

    Add files via upload

--- javascript/quotejs.js
+++ javascript/quotejs.js
@@ -1,41 +1,9 @@
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
 
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
-
-
-function geturlperm()
-{
-	var xhr = createCORSRequest('GET', "https://github.com/neozenweb/CodingChallenge/blob/master/QuotesApi.API");
-	if (!xhr) {
-	  throw new Error('CORS not supported');
-	}
-
-}
 
 
 $(document).ready(function(){
 						   
-						   geturlperm();
+						 
 						   
 					$("button").click(function(){
 													   alert("Button clicked");
