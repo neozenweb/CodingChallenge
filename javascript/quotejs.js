137380bcfde67a2a1f1f2ece1d84852140b74e0d
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-04-05 15:56:37 -0400

    Update quotejs.js

--- javascript/quotejs.js
+++ javascript/quotejs.js
@@ -10,18 +10,18 @@ $(document).ready(function(){
 													
 							$.getJSON("https://raw.githubusercontent.com/neozenweb/CodingChallenge/master/QuotesApi.API",function(result){
 									randomquote = JSON.stringify(result);
-								        
-								      
-								});
-									
-								 
+							    
 								   randnum = Math.floor(Math.random() * 10) +1;
 								    alert("Random number is " + randnum);
 								
 								     alert("Random quote is  " +randomquote[randnum]);
 									ranqt =  randomquote[randnum];
 								  
-																			  
+												        
+								      
+								});
+									
+																  
 													
 													   
 											$("#quotediv").html(ranqt);
