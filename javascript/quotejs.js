e83243a6b2441326c44076b9ca98e43303e43454
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-04-05 17:04:09 -0400

    Update quotejs.js

--- javascript/quotejs.js
+++ javascript/quotejs.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
 									
 							          randomquote =JSON.parse(JSON.stringify(result));
 								   randnum = Math.floor(Math.random() * 10) +1;
-								    alert("Random number is " + randnum);
+								  //  alert("Random number is " + randnum);
 								    
-								alert("Random quote is  " +randomquote[randnum]["quote"] + " AUTHOR  " +randomquote[randnum]["author"]);
+								//alert("Random quote is  " +randomquote[randnum]["quote"] + " AUTHOR  " +randomquote[randnum]["author"]);
 										 
 								     			   
 											$("#quotediv").text(randomquote[randnum]["quote"]);
